637ab68fa585527a69db5ed049f92e526b00d6ca
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2023-07-27 21:46:55 +0200

    Lists of auctions

--- models/auction/currentAuctions.js
+++ models/auction/currentAuctions.js
@@ -1,16 +1,14 @@
 const db = require('../db_connection/connection.js');
-const DateTime = require('luxon');
+const {DateTime} = require('luxon');
 
-function getCurrentAuctions() {
-    let result;
-    let date = DateTime.now().toFormat('y-LL-dd HH:mm:ss');
+function getCurrentAuctions(callback) {
+    let date = DateTime.now().toFormat('yyyy-LL-dd HH:mm:ss');
     db.query(
         `SELECT * FROM auctions WHERE offers_start < '${date}' AND offers_stop > '${date}'`,
         function (err, results) {
-            result = results;
+            callback(results);
         }
     );
-    return result;
 }
 
 module.exports = getCurrentAuctions;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const getCurrentAuctions = require('../models/auction/currentAuctions.js');
+const getFinishedAuctions = require('../models/auction/finishedAuctions.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Aplikacja do obsługi przetargów' });
+});
+
+router.get('/auctions', function(req, res) {
+  getCurrentAuctions(function(data) {
+    res.render('currentAuctions', { title: 'Lista aktualnych przetargów', data: data });
+  });
+});
+
+router.get('/auctions-finished', function(req, res) {
+  getFinishedAuctions(function(data) {
+    res.render('finishedAuctions', { title: 'Lista zakończonych przetargów', data: data });
+  });
 });
 
 module.exports = router;
